04c4410168b73d28f82b6cdaae4b7aec9c14bb16
JP-Ellis <josh@jpellis.me>
2026-03-05 10:21:12 +1100

    chore: second round of updates
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "cypress";
 import registerPactTasks from "./cypress/support/pact-tasks";
 
 export default defineConfig({
+  allowCypressEnv: false,
+  expose: {
+    PACT_PORT: 1234,
+    PACT_DIR: "./pacts",
+    PACT_PROVIDER: process.env.PACT_PROVIDER ?? "",
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.cy.ts",
@@ -9,10 +15,5 @@ export default defineConfig({
       registerPactTasks(on, config);
       return config;
     },
-  },
-  env: {
-    PACT_PORT: 1234,
-    PACT_DIR: "./pacts",
-    PACT_PROVIDER: process.env.PACT_PROVIDER ?? "",
   },
 });
